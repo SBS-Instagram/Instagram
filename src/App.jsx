76bf2fba6e908aa8ab7ca4c44012b4999b62c369
byunghoonyoon <qudgns0218@gmail.com>
2022-09-02 00:59:33 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -124,8 +124,9 @@ export default App;
 // 3.git remote set-url origin https://github.com/byunghoonyoon/Instagram
 // 위 3번으로 인해 원격저장소가 제 git으로 지정됩니다.
 // 4.다시 2번 실행후, git remote set-url origin https://github.com/byunghoonyoon/Instagram 잘나오는지 확인
-// 5.git config --global user.name"Github ID" -> Github Id에 Github 이름쓰세요
-// 6.git config --global user.email "Github Email" -> Github 로그인하는 이메일 쓰세요
+// 5.git config --global user.name"byunghoonyoon" -> Github Id에 Github 이름쓰세요
+// 6.git config --global user.email "qudgns0218@gmail.com" -> Github 로그인하는 이메일 쓰세요
+// 무시하세요 https://github.com/SBS-Instagram/Instagram
 // 7-1. Windows라면 git config --global core.autocrlf true
 // 7-2. Mac이라면 git config --global core.autocrlf input
 // 8. git add .
