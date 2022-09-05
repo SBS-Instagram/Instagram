3be82097f6602b18fa9a6a35dc1afa0968bca6c6
byunghoonyoon <byunghoonyoon@gmail.com>
2022-09-05 14:12:25 +0900

    테스트

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Header = () => {
-<<<<<<< HEAD
   return <div></div>;
-=======
-  return <header></header>;
->>>>>>> bbfcf04fd6e8bad61d0ff5de7f29338bc4872563
 };
 
 export default Header;
