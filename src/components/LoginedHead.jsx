d938d401154ea0d1b3a4925406d511efbb05ee51
byunghoonyoon <qudgns0218@gmail.com>
2022-09-18 21:37:32 +0900

    로그인한 유저와 다른 라우터의 경우 게시글추가 안되게 수정, 오른쪽위 작은 프사는 항상 로그인한 유저의 프사로 나오게 변경

--- src/components/LoginedHead.jsx
+++ src/components/LoginedHead.jsx
@@ -30,6 +30,7 @@ const LoginedHead = ({
   const [searchToggle, setSearchToggle] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const userinfo = JSON.parse(sessionStorage.getItem("user"));
   useEffect(() => {
     //useEffect 사용이유
     //setState는 비동기처리라서, 보류처리된 후 한발짝 늦게 적용된다.
@@ -54,7 +55,7 @@ const LoginedHead = ({
     setSearchToggle(!searchToggle);
   };
 
-  return (
+  return userinfo.userid === user.userid ? (
     <div
       className="Topbar"
       onClick={() => {
@@ -194,6 +195,175 @@ const LoginedHead = ({
                     user.imgSrc == undefined
                       ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_8odrQguUEk4y0r47v-EpBtqpn-Iw3WiErA&usqp=CAU"
                       : user.imgSrc
+                  }
+                  alt=""
+                  onChange={onImageChange}
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex="0"
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a
+                  href="https://www.instagram.com/anjjaaang/"
+                  className="justify-between"
+                >
+                  프로필
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>저장됨</a>
+              </li>
+              <li>
+                <a>설정</a>
+              </li>
+              <li>
+                <a>계정 변환</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      className="Topbar"
+      onClick={() => {
+        if (searchToggle) {
+          setSearchToggle(false);
+          setSearchedList("");
+        }
+      }}
+    >
+      <div className="navbar bg-base-100 Topbar_logo">
+        <div className="flex-1">
+          <a
+            href="http://localhost:3000/"
+            className="btn btn-ghost normal-case text-xl"
+          >
+            instagram
+          </a>
+        </div>
+        <div className="flex-none gap-5">
+          <div className="search relative">
+            <input
+              type="text"
+              placeholder="검색"
+              onChange={onSearchChange}
+              value={searchValue}
+              onClick={() => {
+                setSearchToggle(true);
+              }}
+              onKeyPress={(e) => {
+                if (e.key == "Enter") {
+                  onSearch(searchValue);
+                }
+              }}
+            />
+            <button>
+              <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"></img>
+            </button>
+            {searchToggle && (
+              <div className="searchBox">
+                <span
+                  style={{
+                    fontSize: "1.1rem",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  검색 항목
+                </span>
+                <button
+                  onClick={() => {
+                    onSearchToggle();
+                  }}
+                  className="searchBoxButton"
+                >
+                  <FaWindowClose
+                    style={{
+                      position: "absolute",
+                      right: "2",
+                      top: "2",
+                      fontSize: "1.5rem",
+                    }}
+                  />
+                </button>
+                {searchedList != "" ? (
+                  <div className="searchedBox flex mt-2 ml-1">
+                    {searchedList.map((searched, id) => (
+                      <li key={id}>
+                        <div className="flex gap-1">
+                          <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 img-Box ml-2 mt-1">
+                            <a href="#">
+                              <img
+                                src={
+                                  searched.imgSrc != null
+                                    ? searched.imgSrc
+                                    : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_8odrQguUEk4y0r47v-EpBtqpn-Iw3WiErA&usqp=CAU"
+                                }
+                              />
+                            </a>
+                          </div>
+                          <div>
+                            <div className="searchedName">
+                              <span>
+                                <a href={searched.userid}>
+                                  {searched.username}
+                                </a>
+                              </span>
+                            </div>
+                            <div className="searchedId">
+                              <a href={searched.userid}>
+                                <span>{searched.userid}</span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="flex justify-center align-center">
+                    <span>검색 결과 없음</span>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+          <button>
+            <FaHome style={{ fontSize: "25px" }} />
+          </button>
+          <button>
+            <FaRegComment style={{ fontSize: "25px" }} />
+          </button>
+          <button>
+            <FaPlusSquare style={{ fontSize: "25px" }} />
+          </button>
+
+          <button
+            onClick={() => {
+              if (window.confirm("정말 로그아웃 하시겠습니까?")) {
+                setUser("");
+                sessionStorage.clear();
+                setLogined(!logined);
+                onMoveHompage();
+              }
+            }}
+          >
+            <a href="#">Logout</a>
+          </button>
+
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src={
+                    userinfo.imgSrc == undefined
+                      ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_8odrQguUEk4y0r47v-EpBtqpn-Iw3WiErA&usqp=CAU"
+                      : userinfo.imgSrc
                   }
                   alt=""
                   onChange={onImageChange}
